6fd474f81c71b219e6286c05452584555b8ac115
osama maree <osamamarei2001@gmail.com>
2023-09-23 20:46:14 +0300

    Edit For Code Review

--- main.js
+++ main.js
@@ -1,104 +1,105 @@
-let Attributes = document.getElementsByTagName("tbody"),
-  count = document.getElementsByTagName("p"),
-  input = document.getElementById("input"),
+let attributes = document.getElementsByTagName("tbody"),
+  countTask = document.getElementsByTagName("p"),
+  inputTask = document.getElementById("input"),
   addBtn = document.getElementById("addBtn"),
-  Search = document.getElementById("search"),
-  Todos,
-  Temp = [],
+  searchTask = document.getElementById("search"),
+  todos,
+  temp = [],
   inpEdit = "";
-
-function fetchData() {
-  Todos = JSON.parse(localStorage.getItem("todos"));
-  Temp = Todos;
-  ShowTodo();
-}
-fetchData();
-function ShowTodo() {
+function showTodo() {
   rows = "";
-  Todos?.forEach((element) => {
+  todos?.forEach((element) => {
     rows += "<tr>";
     rows += `<td>${element.id}</td>`;
     rows += `<td style='${
       element.completed ? "text-decoration: line-through;" : ""
     }'>${
       element.edit
-        ? `<input type="text" onkeyup="SaveChange(
+        ? `<input type="text" onkeyup="saveChange(
             this.value,${element.id}
           )" value="${getValue(element.id)}" />`
         : element.todo
     }</td>`;
-    rows += `<td style="cursor:pointer" onclick="ToggleEdit(${element.id})">${
+    rows += `<td style="cursor:pointer" onclick="toggleEdit(${element.id})">${
       element.edit && !element.completed ? "Save" : "Edit"
     }</td>`;
     rows += `<td>${element.userId}</td>`;
     rows += `<td>${element.completed}</td>`;
-    rows += `<td>  <button class="custom-button mb" onclick="DeleteTodo(${element.id})">Delete</button>
-                   <button class="custom-button" style="background-color: #4CAF50;"onclick="DoneTodo(${element.id})">Done</button>
+    rows += `<td>  <button class="custom-button mb" onclick="deleteTodo(${element.id})">Delete</button>
+                   <button class="custom-button" style="background-color: #4CAF50;"onclick="doneTodo(${element.id})">Done</button>
                </td>`;
     rows += "</tr>";
   });
-  Attributes[0].innerHTML = rows;
-  count[0].textContent = `Total tasks: ${Todos.length}`;
+  attributes[0].innerHTML = rows;
+  countTask[0].textContent = `Total tasks: ${todos.length}`;
 }
-function DeleteTodo(id) {
+function fetchData() {
+  todos = JSON.parse(localStorage.getItem("todos"));
+  temp = todos;
+  showTodo();
+}
+fetchData();
+
+function deleteTodo(id) {
   if (confirm("Are you sure?")) {
-    Todos = Todos.filter((todo) => todo.id != id);
-    localStorage.setItem("todos", JSON.stringify(Todos));
+    todos = todos.filter((todo) => todo.id != id);
+    localStorage.setItem("todos", JSON.stringify(todos));
     fetchData();
   }
 }
-function DoneTodo(id) {
-  Todos.forEach((todo) => {
+function doneTodo(id) {
+  todos.forEach((todo) => {
     if (todo.id === id) {
       todo.completed = !todo.completed;
     }
   });
-  localStorage.setItem("todos", JSON.stringify(Todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
   fetchData();
 }
-function ToggleEdit(id) {
-  Todos.forEach((todo) => {
+function toggleEdit(id) {
+  todos.forEach((todo) => {
     if (todo.id === id && !todo.completed) {
       todo.edit = !todo.edit;
     }
   });
-  localStorage.setItem("todos", JSON.stringify(Todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
   fetchData();
 }
-function SaveChange(val, id) {
-  Todos.forEach((todo) => {
+function saveChange(val, id) {
+  todos.forEach((todo) => {
     if (todo.id === id && !todo.completed) {
       todo.todo = val;
     }
   });
 }
 function getValue(id) {
-  for (let i = 0; i < Todos.length; i++) {
-    if (Todos[i].id === id && !Todos[i].completed) {
-      return Todos[i].todo;
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id && !todos[i].completed) {
+      return todos[i].todo;
     }
   }
 }
 addBtn.onclick = function (e) {
   e.preventDefault();
-  if (!input.value) {
+  if (!inputTask.value) {
     return alert("Please fill input");
   }
   const todo = {
-    id: Todos?.length > 0 ? Todos[Todos.length - 1].id + 1 : 0,
+    id: todos?.length > 0 ? todos[todos.length - 1].id + 1 : 0,
     userId: parseInt(Math.random() * (1000 - 1) + 1),
     edit: false,
     completed: false,
-    todo: input.value,
+    todo: inputTask.value,
   };
-  Todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(Todos));
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  inputTask.value = "";
   fetchData();
 };
-Search.onkeyup = function () {
-  Todos = Temp;
-  Todos = Todos.filter((todo) =>
-    todo.todo.toUpperCase().startsWith(Search.value.toUpperCase())
+searchTask.onkeyup = function () {
+  todos = temp;
+  todos = todos.filter((todo) =>
+    todo.todo.toUpperCase().startsWith(searchTask.value.toUpperCase())
   );
-  ShowTodo();
+  showTodo();
 };
